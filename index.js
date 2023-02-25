886af8079e450e43c0795c13473d251710790ef5
+ <+>
2023-02-24 22:31:54 -0500

    done

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+let random = 5;
+Math.floor(Math.random()) * 10;
+
+let num3 = 8782
+let num4 = 42
+let mod =  num3 % num4;
+;
+ 
+const max = (1, 2, 3, 4, 5, 20);
